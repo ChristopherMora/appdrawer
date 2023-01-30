fb41981ff70aa806c3694095933dd53cf9fb0cf8
ChristopherMora <95389955+ChristopherMora@users.noreply.github.com>
2023-01-29 23:13:04 -0600

    drawer

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'actividad3/listas.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Listas()
+    );
+  }
+}
